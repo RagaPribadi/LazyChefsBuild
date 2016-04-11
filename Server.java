9ad349d2de9069691623e73261fd666b010b5f97
Yijia Cui <yijiacui408@gmail.com>
2016-04-11 01:40:04 -0400

    Server code
    Add Server code to the server

---
+++ Server.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class Server {
+
+    public static void main(String[] args) throws Exception
+    {
+        //default port number is 9090
+        ServerSocket listener = new ServerSocket(9090);
+        try {
+            while (true) {
+                Socket socket = listener.accept();
+                try {
+                   ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+                   //convert ObjectInputStream object to String
+                   String keyword = (String) ois.readObject();
+                   System.out.println("Keyword Received: " + keyword);
+
+                  String message = "";
+                  int counter = 0;
+                  BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("db.txt")));
+                  try {
+                      String line;
+                      while ((line = br.readLine()) != null) {
+                          // process line
+                        if(line.contains(keyword) && counter < 6)
+                        {
+                           message += (line + "\n"); //check whether the line read in contains search key word 
+                           counter++;
+                        }
+                      }
+                  } finally {
+                      br.close();
+                  }
+                   
+                   //create ObjectOutputStream object
+                  ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+                  //write object to Socket
+                  oos.writeObject(message);
+                  //close resources
+                  ois.close();
+                  oos.close();
+                } finally {
+                    socket.close();
+                }
+            }
+        }
+        finally {
+            listener.close();
+        }
+    }
+}
